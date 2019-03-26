9159458840637f7894d3076379bc7809efd98ac6
Wagner Mattos <wagner_mattos@msn.com>
2019-03-26 10:26:26 -0300

    feat: add input robot and state robot

--- index.js
+++ index.js
@@ -1,15 +1,23 @@
 const robots = {
     input: require('./robots/input.js'),
-    text: require('./robots/text.js')
+    text: require('./robots/text.js'),
+    state: require('./robots/state.js')
 }
 
 async function start() {
 
     robots.input()
-    //await robots.text(content)
+    await robots.text()
 
+    showContent()
 
-    //console.log(JSON.stringify(content, null, 4))
+}
+
+function showContent() {
+    const content = robots.state.load()
+    console.dir(content, {
+        depth: null
+    })
 }
 
 start()
