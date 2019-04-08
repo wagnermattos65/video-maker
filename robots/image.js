d514c51790d6a74d52a8ef731f6113c8d25d1a84
Wagner Mattos <wagner_mattos@msn.com>
2019-04-08 18:59:41 -0300

    feat: add image download logic with fallback and blacklisted urls

--- robots/image.js
+++ robots/image.js
@@ -1,3 +1,4 @@
+const imageDownloader = require('image-downloader')
 const google = require('googleapis').google
 const customSearch = google.customsearch('v1')
 const state = require("./state")
@@ -7,7 +8,9 @@ const googleSearchCredentials = require('../credentials/google_search.json')
 async function robot() {
     const content = state.load()
 
+    await updateContentImageBlacklist();
     await fetchImagesOfAllSentences(content)
+    await downloadAllImages(content)
     state.save(content)
 
     async function fetchImagesOfAllSentences(content) {
@@ -26,7 +29,8 @@ async function robot() {
             q: query,
             searchType: 'image',
             imgSize: 'huge',
-            num: 2
+            rights: 'cc_publicdomain',
+            num: 5
         })
 
         const imageUrl = response.data.items.map(item => {
@@ -34,9 +38,52 @@ async function robot() {
         })
 
         return imageUrl
-
     }
 
+    async function downloadAllImages(content) {
+        content.downloadedImages = []
+        for (let sentencesIndex = 0; sentencesIndex < content.sentences.length; sentencesIndex++) {
+            const images = content.sentences[sentencesIndex].images
+            for (let imageIndex = 0; imageIndex < images.length; imageIndex++) {
+                const imageUrl = images[imageIndex]
+
+                try {
+                    if (content.downloadedImages.includes(imageUrl)) {
+                        throw new Error('Imagem já foi baixada.')
+                    }
+                    if (content.blacklistedImages.includes(imageUrl)) {
+                        throw new Error('Blacklisted Image refused.')
+                    }
+                    await downloadAndSave(imageUrl, `${sentencesIndex}-original.png`)
+                    content.downloadedImages.push(imageUrl)
+                    console.log(`>>> [${sentencesIndex}][${imageIndex}]:Baixou imagem com sucesso: ${imageUrl}`)
+                    break
+                } catch (err) {
+                    console.log(`>>> [${sentencesIndex}][${imageIndex}]:Erro ao baixar (${imageUrl}): ${err}`)
+                }
+            }
+        }
+    }
+
+    async function downloadAndSave(url, fileName) {
+        return imageDownloader.image({
+            url,
+            url,
+            dest: `./content/${fileName}`
+        })
+    }
+
+    async function updateContentImageBlacklist() {
+        const imageBlacklist = state.loadBlacklist()
+        for (image in imageBlacklist) {
+            if (!content.blacklistedImages) {
+                content.blacklistedImages = []
+            }
+            if (!content.blacklistedImages.includes(imageBlacklist[image])) {
+                content.blacklistedImages.push(imageBlacklist[image])
+            }
+        }
+    }
 }
 
 module.exports = robot
